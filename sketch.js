518a1343fda5836d9ea10f658bae824c1408f4de
Kerem-Y <kerem184454@gmail.com>
2026-05-12 22:46:22 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -41,7 +41,7 @@ function gotHands(results) {
 function draw() {
   if (!isStarted) {
     background(0);
-    image(startImage, 30, 50, 1300, 1010);
+    image(startImage, 80, 100, 1300, 1010);
     return;
   }
 
